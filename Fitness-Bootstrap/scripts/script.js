0c61680532887a8d9822a0d7124756e91fc84f00
Colby993 <122412306+Colby993@users.noreply.github.com>
2023-04-23 20:56:45 -0500

    n

---
+++ Fitness-Bootstrap/scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name:colby stewart
+    File Name: script.js
+    Date: 4/12/2022
+*/
+
+//jQuery for hero image to consume the header window space
+$(document).ready(function(){
+    $('.hero').height($(window).height());
+});
