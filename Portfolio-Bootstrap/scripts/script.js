27cc0a26757d4a68c02c702fb82dc4200a5f0a59
Colby993 <122412306+Colby993@users.noreply.github.com>
2023-04-25 15:01:10 -0500

    new

--- Portfolio-Bootstrap/scripts/script.js
+++ Portfolio-Bootstrap/scripts/script.js
@@ -8,3 +8,20 @@
 $(document).ready(function(){
     $('.hero').height($(window).height());
 });
+window.onload = function() {
+    var userAgent = navigator.userAgent;
+    var language = navigator.language;
+    var platform = navigator.platform;
+
+    var screenWidth = screen.width;
+    var screenHeight = screen.height;
+    var colorDepth =  screen.colorDepth;
+
+
+    document.getElementById("userAgent").innerHTML = "User agent: "+ userAgent;
+    document.getElementById("language").innerHTML = "Language: "+ language;
+    document.getElementById("platform").innerHTML = "Platform: "+ platform;
+    document.getElementById("screenWidth").innerHTML = "Screen width: "+ screenWidth;
+    document.getElementById("screenHeight").innerHTML = "Screen height: "+ screenHeight;
+    document.getElementById("colorDepth").innerHTML = "Screen color depth: "+ colorDepth;
+}
